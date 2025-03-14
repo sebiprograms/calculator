5a39ce5d84907fc3b7666d83de637638da451d4f
Sebastian Stockton <96149632+sebiprograms@users.noreply.github.com>
2025-03-14 18:05:28 -0500

    Clear func
    Cleans vars and display

--- index.js
+++ index.js
@@ -19,7 +19,16 @@ function subtract(var1,var2) {
     return var1-var2
 }
 
+// Resets vars and display
+function clear() {
+    const display = document.querySelector("#display")
+    var1 = 0
+    var2 = 0
+    operator = ""
+    display.textContent = ""
+}
 
+// operator recieved from button id, then called on eventlistener
 function operate(var1, operator, var2) {
     if (operator == "*") {
         multiply(var1, var2)
